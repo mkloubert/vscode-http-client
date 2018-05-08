eb89c13c598ff7a0c0ac3896afe327fd432784ce
Marcel Kloubert <marcel.kloubert@gmx.net>
2018-05-08 08:03:30 +0200

    minor changes

--- src/requests.ts
+++ src/requests.ts
@@ -951,7 +951,7 @@ export class HTTPRequest extends HTTPRequestBase {
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header bg-primary text-white">
-        <h5 class="modal-title">Edit URL parameters</h5>
+        <h5 class="modal-title">Edit URL Parameters</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true" class="text-white">&times;</span>
         </button>
